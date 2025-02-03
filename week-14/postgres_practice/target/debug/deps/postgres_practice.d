86a243c27d42241e73bdbeb68f9157f3dcdeb119
David Alamutu <davidalamutu@gmail.com>
2025-02-03 22:55:12 +0100

    (postgresSQL commit)

---
+++ week-14/postgres_practice/target/debug/deps/postgres_practice.d
@@ -0,0 +1,5 @@
+C:\Users\ALAMUTU\Documents\d.alamutuCOS101\week-14\postgres_practice\target\debug\deps\postgres_practice.exe: src/main.rs
+
+C:\Users\ALAMUTU\Documents\d.alamutuCOS101\week-14\postgres_practice\target\debug\deps\postgres_practice.d: src/main.rs
+
+src/main.rs:
---
+++ week-14/postgres_practice/target/debug/postgres_practice.d
@@ -0,0 +1,1 @@
+C:\Users\ALAMUTU\Documents\d.alamutuCOS101\week-14\postgres_practice\target\debug\postgres_practice.exe: C:\Users\ALAMUTU\Documents\d.alamutuCOS101\week-14\postgres_practice\src\main.rs
